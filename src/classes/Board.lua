a73b3a1026f3c784ad8b2cea2b5fa3245ad92e8d
Daniel de Sousa <daniel.lopes04@gmail.com>
2021-12-29 20:34:22 -0300

    Calculating matches and replacing tiles

--- src/classes/Board.lua
+++ src/classes/Board.lua
@@ -41,10 +41,212 @@ function Board:initializeTiles()
             table.insert(self.tiles[tileY], Tile(
                 tileX, tileY,
                 math.random(18),
-                math.random(6)
+                math.random(1)
             ))
         end
     end
+end
+
+--[[
+    Goes left to right, top to bottom in the board, calculating matches by counting consecutive
+    tiles of the same color. Doesn't need to check the last tile in every row or column if the 
+    last two haven't been a match.
+]]
+function Board:calculateMatches()
+    local matches = {}
+
+    -- How many of the same color blocks in a row we've found
+    local matchNum = 1
+
+    -- Horizontal matches first
+    for y = 1, 8 do
+        local colorToMatch = self.tiles[y][1].color
+
+        matchNum = 1
+        
+        -- Every horizontal tile
+        for x = 2, 8 do
+            -- If this is the same color as the one we're trying to match...
+            if self.tiles[y][x].color == colorToMatch then
+                matchNum = matchNum + 1
+            else
+                -- Set this as the new color we want to watch for
+                colorToMatch = self.tiles[y][x].color
+
+                -- If we have a match of 3 or more up to now, add it to our matches table
+                if matchNum >= 3 then
+                    local match = {}
+
+                    -- Go backwards from here by matchNum
+                    for x2 = x - 1, x - matchNum, -1 do
+                        -- Add each tile to the match that's in that match
+                        table.insert(match, self.tiles[y][x2])
+                    end
+
+                    -- Add this match to our total matches table
+                    table.insert(matches, match)
+                end
+
+                -- Don't need to check last two if they won't be in a match
+                if x >= 7 then
+                    break
+                end
+
+                matchNum = 1
+            end
+        end
+
+        -- Account for the last row ending with a match
+        if matchNum >= 3 then
+            local match = {}
+            
+            -- Go backwards from end of last row by matchNum
+            for x = 8, 8 - matchNum + 1, -1 do
+                table.insert(match, self.tiles[y][x])
+            end
+
+            table.insert(matches, match)
+        end
+    end
+
+    -- Vertical matches
+    for x = 1, 8 do
+        local colorToMatch = self.tiles[1][x].color
+
+        matchNum = 1
+
+        -- Every vertical tile
+        for y = 2, 8 do
+            if self.tiles[y][x].color == colorToMatch then
+                matchNum = matchNum + 1
+            else
+                colorToMatch = self.tiles[y][x].color
+
+                if matchNum >= 3 then
+                    local match = {}
+
+                    for y2 = y - 1, y - matchNum, -1 do
+                        table.insert(match, self.tiles[y2][x])
+                    end
+
+                    table.insert(matches, match)
+                end
+
+                matchNum = 1
+
+                -- Don't need to check last two if they won't be in a match
+                if y >= 7 then
+                    break
+                end
+            end
+        end
+
+        -- Account for the last column ending with a match
+        if matchNum >= 3 then
+            local match = {}
+            
+            -- Go backwards from end of last row by matchNum
+            for y = 8, 8 - matchNum, -1 do
+                table.insert(match, self.tiles[y][x])
+            end
+
+            table.insert(matches, match)
+        end
+    end
+
+    -- Store matches for later reference
+    self.matches = matches
+
+    return #self.matches > 0 and self.matches or false
+end
+
+--[[
+    Remove the matches from the Board by just setting the Tile slots within them to nil,
+    then setting self.matches to nil.
+]]
+function Board:removeMatches()
+    for k, match in pairs(self.matches) do
+        for k, tile in pairs(match) do
+            self.tiles[tile.gridY][tile.gridX] = nil
+        end
+    end
+
+    self.matches = nil
+end
+
+--[[
+    Shifts down all of the tiles that now have spaces below them, then returns a table that
+    contains tweening information for these new tiles.
+]]
+function Board:getFallingTiles()
+    -- Tween table, with tiles as keys and their x and y as the to values
+    local tweens = {}
+
+    -- For each column, go up tile by tile till we hit a space
+    for x = 1, 8 do
+        local space = false
+        local spaceY = 0
+
+        local y = 8
+        while y >= 1 do
+            -- If our last tile was a space...
+            local tile = self.tiles[y][x]
+            
+            if space then
+                -- If the current tile is *not* a space, bring this down to the lowest space
+                if tile then
+                    -- Put the tile in the correct spot in the board and fix its grid positions
+                    self.tiles[spaceY][x] = tile
+                    tile.gridY = spaceY
+
+                    -- Set its prior position to nil
+                    self.tiles[y][x] = nil
+
+                    -- Tween the Y position to 32 x its grid position
+                    tweens[tile] = {
+                        y = (tile.gridY - 1) * 32
+                    }
+
+                    -- Set space back to 0, set Y to spaceY so we start back from here again
+                    space = false
+                    y = spaceY
+                    spaceY = 0
+                end
+            elseif tile == nil then
+                space = true
+                
+                if spaceY == 0 then
+                    spaceY = y
+                end
+            end
+
+            y = y - 1
+        end
+    end
+
+    -- Create replacement tiles at the top of the screen
+    for x = 1, 8 do
+        for y = 8, 1, -1 do
+            local tile = self.tiles[y][x]
+
+            -- If the tile is nil, we need to add a new one
+            if not tile then
+                local tile = Tile(x, y, math.random(18), math.random(1))
+                tile.y = -32
+                self.tiles[y][x] = tile
+
+                tweens[tile] = {
+                    y = (tile.gridY - 1) * 32
+                }
+            end
+        end
+    end
+
+    return tweens
+end
+
+function Board:getNewTiles()
+    return {}
 end
 
 function Board:render()
--- src/states/PlayState.lua
+++ src/states/PlayState.lua
@@ -22,9 +22,14 @@ function PlayState:enter()
     self.offsetY = 16
     self.board = Board(self.offsetX, self.offsetY)
 
-    -- Tile selected to be swapped
+    -- Flag to show whether we're able to process input (not swapping or clearing)
+    self.canInput = true
+    
+    -- tile we're currently highlighting (preparing to swap)
     self.highlightedTile = nil
-    self.highlightedX, self.highlightedY = 1, 1
+    self.highlightedX, self.highlightedY = 0, 0
+
+    self.score = 0
 
     -- Current selected tile, changed with arrow keys
     self.selectedTile = self.board.tiles[1][1]
@@ -33,58 +38,64 @@ end
 function PlayState:update(dt)
     local x, y = self.selectedTile.gridX, self.selectedTile.gridY
 
-    -- Moving the selected tile
-    if love.keyboard.wasPressed('up') then
-        if y > 1 then
-            self.selectedTile = self.board.tiles[y - 1][x]
+    if self.canInput then
+        -- Moving the selected tile
+        if love.keyboard.wasPressed('up') then
+            if y > 1 then
+                self.selectedTile = self.board.tiles[y - 1][x]
+            end
+        elseif love.keyboard.wasPressed('down') then
+            if y < 8 then
+                self.selectedTile = self.board.tiles[y + 1][x]
+            end
+        elseif love.keyboard.wasPressed('left') then
+            if x > 1 then
+                self.selectedTile = self.board.tiles[y][x - 1]
+            end
+        elseif love.keyboard.wasPressed('right') then
+            if x < 8 then
+                self.selectedTile = self.board.tiles[y][x + 1]
+            end
         end
-    elseif love.keyboard.wasPressed('down') then
-        if y < 8 then
-            self.selectedTile = self.board.tiles[y + 1][x]
-        end
-    elseif love.keyboard.wasPressed('left') then
-        if x > 1 then
-            self.selectedTile = self.board.tiles[y][x - 1]
-        end
-    elseif love.keyboard.wasPressed('right') then
-        if x < 8 then
-            self.selectedTile = self.board.tiles[y][x + 1]
-        end
-    end
-    
-    -- When enter is pressed we highlight a tile
-    if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
-        if not self.highlightedTile then
-            self.highlightedTile = true
-            self.highlightedX, self.highlightedY = self.selectedTile.gridX, self.selectedTile.gridY
-            self.highlightedTile = self.board.tiles[y][x]
-        -- if we select the position already highlighted, remove highlight
-        elseif self.highlightedTile == self.board.tiles[y][x] then
-            self.highlightedTile = nil
-        elseif math.abs(self.highlightedX - self.selectedTile.gridX) + math.abs(self.highlightedY - self.selectedTile.gridY) > 1 then
-            self.highlightedTile = nil
-        else
-            -- Swap grid positions of tiles
-            local tempX, tempY = self.highlightedTile.gridX, self.highlightedTile.gridY
-            local newTile = self.board.tiles[y][x]
+        
+        -- When enter is pressed we highlight a tile
+        if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
+            if not self.highlightedTile then
+                self.highlightedTile = true
+                self.highlightedX, self.highlightedY = self.selectedTile.gridX, self.selectedTile.gridY
+                self.highlightedTile = self.board.tiles[y][x]
+            -- if we select the position already highlighted, remove highlight
+            elseif self.highlightedTile == self.board.tiles[y][x] then
+                self.highlightedTile = nil
+            elseif math.abs(self.highlightedX - self.selectedTile.gridX) + math.abs(self.highlightedY - self.selectedTile.gridY) > 1 then
+                self.highlightedTile = nil
+            else
+                -- Swap grid positions of tiles
+                local tempX, tempY = self.highlightedTile.gridX, self.highlightedTile.gridY
+                local newTile = self.board.tiles[y][x]
 
-            self.highlightedTile.gridX = newTile.gridX
-            self.highlightedTile.gridY = newTile.gridY
-            newTile.gridX = tempX
-            newTile.gridY = tempY
+                self.highlightedTile.gridX = newTile.gridX
+                self.highlightedTile.gridY = newTile.gridY
+                newTile.gridX = tempX
+                newTile.gridY = tempY
 
-            -- Swap tiles in the tiles table
-            self.board.tiles[self.highlightedTile.gridY][self.highlightedTile.gridX] = self.highlightedTile
-            self.board.tiles[newTile.gridY][newTile.gridX] = newTile
+                -- Swap tiles in the tiles table
+                self.board.tiles[self.highlightedTile.gridY][self.highlightedTile.gridX] = self.highlightedTile
+                self.board.tiles[newTile.gridY][newTile.gridX] = newTile
 
-            -- Tween coordinates between the two so they swap
-            Timer.tween(0.2, {
-                [self.highlightedTile] = {x = newTile.x, y = newTile.y},
-                [newTile] = {x = self.highlightedTile.x, y = self.highlightedTile.y}
-            })
+                -- Tween coordinates between the two so they swap
+                Timer.tween(0.1, {
+                    [self.highlightedTile] = {x = newTile.x, y = newTile.y},
+                    [newTile] = {x = self.highlightedTile.x, y = self.highlightedTile.y}
+                })
+                -- Once the swap is finished, we can tween falling blocks as needed
+                :finish(function()
+                    self:calculateMatches()
+                end)
 
-            self.selectedTile = self.highlightedTile
-            self.highlightedTile = nil
+                self.selectedTile = self.highlightedTile
+                self.highlightedTile = nil
+            end
         end
     end
 
@@ -93,6 +104,51 @@ function PlayState:update(dt)
     end
 
     Timer.update(dt)
+end
+
+--[[
+    Calculates whether any matches were found on the board and tweens the needed
+    tiles to their new destinations if so. Also removes tiles from the board that
+    have matched and replaces them with new randomized tiles, deferring most of this
+    to the Board class.
+]]
+function PlayState:calculateMatches()
+    self.highlightedTile = nil
+
+    -- If we have any matches, remove them and tween the falling blocks that result
+    local matches = self.board:calculateMatches()
+
+    if matches then
+        -- gSounds['match']:stop()
+        -- gSounds['match']:play()
+
+        -- Add score for each match
+        for k, match in pairs(matches) do
+            self.score = self.score + #match * 50
+        end
+
+        -- Remove any tiles that matched from the board
+        self.board:removeMatches()
+
+        -- Gets a table with tween values for tiles that should now fall
+        local tilesToFall = self.board:getFallingTiles()
+
+        -- First, tween the falling tiles over 0.25s
+        Timer.tween(0.25, tilesToFall):finish(function()
+            local newTiles = self.board:getNewTiles()
+            
+            -- Then, tween new tiles that spawn from the ceiling over 0.25s to fill in
+            -- the new upper gaps that exist
+            Timer.tween(0.25, newTiles):finish(function()
+                -- Recursively call function in case new matches have been created
+                -- as a result of falling blocks once new blocks have finished falling
+                self:calculateMatches()
+            end)
+        end)
+    -- If no matches, we can continue playing
+    else
+        self.canInput = true
+    end
 end
 
 function PlayState:render()
